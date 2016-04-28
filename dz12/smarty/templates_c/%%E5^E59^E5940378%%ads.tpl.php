c04630f2ec3653cc008d7c0931d415d3c2660d65
Nikolay <nickgran@yandex.ru>
2016-04-28 23:09:49 +0300

    blablabla

---
+++ dz12/smarty/templates_c/%%E5^E59^E5940378%%ads.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version 2.6.25-dev, created on 2016-04-29 00:09:25
+         compiled from ads.tpl */ ?>
+<div class="col-md-offset-1 col-md-5">
+    <h2 class="sub-header">Объявления</h2>
+    <form class="form-inline" method="post">
+            <input type="text" class="form-control" name="text" value='<?php echo $this->_tpl_vars['search']; ?>
+'>
+            <input type="submit" class="btn btn-default" name="search" value="search">
+    </form>
+    <div class="table-condensed">
+        <table class="table table-condensed">
+            <thead>
+                <th>#id</th>
+                <th>Название</th>
+                <th>Цена</th>
+                <th>Имя</th>
+                <th>Действия</th>
+            </thead>
+            <tbody>
+                <?php echo $this->_tpl_vars['ads_rows']; ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
